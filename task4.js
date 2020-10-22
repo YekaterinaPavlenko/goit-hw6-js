811d7f557a595ceb97fc6080185a662444ad61eb
YekaterinaPavlenko <pavluka1605@gmail.com>
2020-10-22 15:07:54 +0300

    добавляет 3,4,5,6,7 задачи

--- task4.js
+++ task4.js
@@ -85,45 +85,56 @@ const users = [
     }
 ]
 // Write code under this line
-const getUsersWithAge = (array, min, max) => array
- .filter(({age}) => age > min && age < max)   
- .map(({name, email}) => ({['name']: name, ['email']: email}));  
+const getInactiveUsers = array  => array.filter(user => !user.isActive);
+function getNotActive ({isActive}) {return isActive;}
 
- console.log(getUsersWithAge(users, 20, 30));
+ console.log(getInactiveUsers(users));
+
 /* [
-    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
-    { name: 'Elma Head', email: 'elmahead@omatom.com' },
-    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
-] */
+  {
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  }     
+]; */
 
- console.log(getUsersWithAge(users, 30, 40));
-/* [
-    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
-    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
-    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
-    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
-] */
+// Задача 6-4
+// filter и оператор !
+// Получи массив только неактивных пользователей (отфильтруй по значению свойства isActive)
 
-// Задача 6-6
-// filter, map и создание анонимных объектов
-// Получи массив из объектов, которые состоят только из свойств name и email тех пользователей, которые попадают в возрастную категорию от min до max лет (значение свойства age).
+// Используй деструктурирующее присваивание для параметра функции ({isActive}) без пробелов и переносов на новую строку.
 
-// Сравнение, пример
+// Используй оператор !.
 
-// const x = 13;
-
-// // num больше 10 ?
-// const larger = num > 10; // true
-
-// // num меньше 100 ?
-// const smaller = 100 > num; // true
-
-// // num находитя между 10 и 100 ?
-// const between = smaller && larger; // true
-// Создание анонимных объектов:
-
-// const arr = Array(3)
-//   .fill('')
-//   .map((e,i) => ({['index']: i }));
-// /* [ { index: 0 }, { index: 1 }, { index: 2 } ] */
 // Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
--- task7.js
+++ task7.js
@@ -85,45 +85,16 @@ const users = [
     }
 ]
 // Write code under this line
-const getUsersWithAge = (array, min, max) => array
- .filter(({age}) => age > min && age < max)   
- .map(({name, email}) => ({['name']: name, ['email']: email}));  
+ const calculateTotalBalance = array => array.reduce((totalBalance, user) => totalBalance + user.balance, 0); 
+function getBalance ({balance}) {return balance;};
 
- console.log(getUsersWithAge(users, 20, 30));
-/* [
-    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
-    { name: 'Elma Head', email: 'elmahead@omatom.com' },
-    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
-] */
+ console.log(calculateTotalBalance(users)); 
+// 20916
 
- console.log(getUsersWithAge(users, 30, 40));
-/* [
-    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
-    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
-    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
-    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
-] */
+// Задача 6-7
+// reduce
+// Получи общую сумму баланса (сумму значений свойства balance) всех пользователей.
 
-// Задача 6-6
-// filter, map и создание анонимных объектов
-// Получи массив из объектов, которые состоят только из свойств name и email тех пользователей, которые попадают в возрастную категорию от min до max лет (значение свойства age).
+// Используй деструктурирующее присваивание для параметра функции {balance} без пробелов и переносов на новую строку
 
-// Сравнение, пример
-
-// const x = 13;
-
-// // num больше 10 ?
-// const larger = num > 10; // true
-
-// // num меньше 100 ?
-// const smaller = 100 > num; // true
-
-// // num находитя между 10 и 100 ?
-// const between = smaller && larger; // true
-// Создание анонимных объектов:
-
-// const arr = Array(3)
-//   .fill('')
-//   .map((e,i) => ({['index']: i }));
-// /* [ { index: 0 }, { index: 1 }, { index: 2 } ] */
 // Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
